73d81d101e34cb119690a59dc63ba274e6eb1168
Pallabi Das <pallabi.das@sgate.net>
2025-01-08 16:46:00 +0530

    changes saved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import { assets } from "../assets/frontend_assets/assets";
 const Footer = () => {
   return (
     <div>
+       <hr />
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-30 text-sm mt-10">
         <div>
           <img src={assets.logo} className="mb-5 w-32" alt="" />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox"; 
 
 const About = () => {
   return (
     <div>
-      
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col sm:flex-row gap-16">
+        <img
+          src={assets.about_img}
+          alt=""
+          className="w-full md:max-w-[450px] "
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Forever was born out of a passion for innovation and a desire to
+            revolutionize the way people shop online. Our journey began with a
+            simple idea: to provide a platform where customers can easily
+            discover,explore and purchase a wide range of products from the
+            comfort of their homes
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <b className="text-gray-800">OUR Mission</b>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+       
+      </div>
+      <div className="text-xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="flex flex-col sm:flex-row text-sm mb-2">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+        <b>Quality Assurance:</b>
+        <p className="text-sm text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+        <b>Convenience:</b>
+        <p className="text-sm text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+        <b>Exceptional Customer Service:</b>
+        <p className="text-sm text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+      </div>
+      <NewsletterBox />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
